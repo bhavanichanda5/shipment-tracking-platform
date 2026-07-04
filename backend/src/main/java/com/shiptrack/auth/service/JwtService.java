6ae1052f93399662768f45ee5bf57b3359456f51
Bhavani Chanda <bhavanichanda5@gmail.com>
2026-07-04 16:43:27 +0530

    Configured JWT Security Filter Chain

--- backend/src/main/java/com/shiptrack/auth/service/JwtService.java
+++ backend/src/main/java/com/shiptrack/auth/service/JwtService.java
@@ -6,8 +6,12 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 
-import java.security.Key;
+import javax.crypto.SecretKey;
 import java.util.Date;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+
 
 @Service
 public class JwtService     
@@ -15,9 +19,11 @@ public class JwtService
     private static final String SECRET_KEY = 
                                              "mySecretKeyForJwtAuthentication12345678901234567890";  
 
-    private final Key key = Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+    private final SecretKey key =
+        Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
 
-    public String generateToken(String username) {
+    public String generateToken(String username) 
+    {
 
     return Jwts.builder()                                                                                                //This starts building a new JWT
             .subject(username)                                                                                           //Who owns this token?
@@ -26,5 +32,37 @@ public class JwtService
             .signWith(key, SignatureAlgorithm.HS256)                                                        //this is used to sign the JWT using the specified algorithm and key. This ensures that the token cannot be tampered(manipulate) with
             .compact();                                                                                                         //This finalizes the building process and returns the JWT as a string
 
-}
+    }
+
+    public String extractUsername(String token) {
+
+    return extractAllClaims(token).getSubject();
+
+    }
+
+   private Claims extractAllClaims(String token) {
+
+        Jws<Claims> claims = Jwts.parser()
+                .verifyWith(key)
+                .build()
+                .parseSignedClaims(token);
+
+    return claims.getPayload();
+
+    }
+
+    public Date extractExpiration(String token) {
+
+    return extractAllClaims(token).getExpiration();
+
+    }
+
+    public boolean isTokenValid(String token, String username) {
+
+    String extractedUsername = extractUsername(token);
+
+    return extractedUsername.equals(username)
+            && !extractExpiration(token).before(new Date());
+
+    }
 }
--- backend/src/main/java/com/shiptrack/config/SecurityConfig.java
+++ backend/src/main/java/com/shiptrack/config/SecurityConfig.java
@@ -1,14 +1,23 @@
 package com.shiptrack.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
 
 @Configuration
 public class SecurityConfig {
+
+    @Autowired
+    private  JwtAuthFilter jwtAuthFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {        
@@ -19,6 +28,16 @@ public class SecurityConfig {
                     .requestMatchers("/api/auth/**").permitAll()
                     .anyRequest().authenticated()
             )
+
+            .sessionManagement(session ->
+
+                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)                                  //JWT doesn't use sessions. Every request carries its own token.So we tell Spring, Don't create sessions.This is exactly how modern REST APIs work
+
+            )
+
+            .addFilterBefore(jwtAuthFilter,
+                    UsernamePasswordAuthenticationFilter.class)                                                 //This line is the heart of JWT Security
+
             .formLogin(form -> form.disable())
             .httpBasic(httpBasic -> httpBasic.disable());
 
@@ -28,5 +47,13 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(
+            AuthenticationConfiguration configuration)
+            throws Exception {
+
+        return configuration.getAuthenticationManager();
     }
 }
